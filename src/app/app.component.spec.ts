a7a2585aa5cee20f6194f9fbf91642359897c1ac
ckruhs <ckruhs@outlook.com>
2018-05-01 15:09:06 +0200

    Basic test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,41 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { ThumbnailPipe } from './pipes/thumbnail.pipe';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule, MatExpansionModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FeedService } from './services/feed.service';
+
 describe('AppComponent', () => {
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        BrowserModule,
+        HttpModule,
+        MatToolbarModule,
+        MatExpansionModule,
+        NoopAnimationsModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        SpinnerComponent,
+        ThumbnailPipe
+      ],
+      providers: [
+        FeedService
       ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'ck'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ck');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ck!');
-  }));
+
 });
