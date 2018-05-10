4ae76ff40e294e8e928c2e9cbfa9d411ba0ca775
ckruhs <ckruhs@outlook.com>
2018-05-10 15:36:26 +0200

    Upgrade to Angular 6

--- src/app/services/feed.service.ts
+++ src/app/services/feed.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/delay';
+
 
 import * as xml2js from 'xml2js';
 
