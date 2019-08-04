fe30c648bd795faacc94f75f03c0d2953c7ff4bf
ckruhs <ckruhs@outlook.com>
2019-08-04 15:43:49 +0200

    Updated to Angular 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { ServiceWorkerModule } from '@angular/service-worker';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule, MatExpansionModule } from '@angular/material';
 
 import { FeedService } from './services/feed.service';
 import { SpinnerComponent } from './spinner/spinner.component';
 import { ThumbnailPipe } from './pipes/thumbnail.pipe';
+
 
 @NgModule({
   declarations: [
@@ -24,10 +25,10 @@ import { ThumbnailPipe } from './pipes/thumbnail.pipe';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule,
     MatToolbarModule,
     MatExpansionModule,
-    NoopAnimationsModule
   ],
   providers: [
     FeedService
