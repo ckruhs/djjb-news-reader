f30c144a101b6aa7d8ba55e5eb517850a1e87c45
ckruhs <ckruhs@users.noreply.github.com>
2020-09-11 09:16:59 +0200

    New layout and add courseplan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { FeedService } from './services/feed.service';
 import { SpinnerComponent } from './spinner/spinner.component';
@@ -30,6 +31,7 @@ import { ThumbnailPipe } from './pipes/thumbnail.pipe';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatExpansionModule,
+    MatTabsModule
   ],
   providers: [
     FeedService
