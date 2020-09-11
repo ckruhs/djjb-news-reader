36cbc334a6b12bf3be39f4dbe90ccfc6a4794ee8
ckruhs <ckruhs@users.noreply.github.com>
2020-09-11 09:16:59 +0200

    New layout and add courseplan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ElementRef, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewEncapsulation, Renderer2, ViewChild } from '@angular/core';
 
 import { delay } from 'rxjs/internal/operators';
 
@@ -14,15 +14,19 @@ import { environment } from '../environments/environment';
               '../assets/addtohomescreen.css'],
   encapsulation: ViewEncapsulation.None
 })
+
 export class AppComponent implements OnInit, AfterViewInit {
   feedLocation = environment.feedLocation + '?v=' + Math.random();  // prevent browser caching
 
   title: string;
   feeds: Array<FeedEntry> = [];
+  
+  @ViewChild('lghost') lghost: ElementRef;
 
   constructor(
     private feedService: FeedService,
-    private elementRef: ElementRef
+    private elementRef: ElementRef,
+    private renderer: Renderer2
   ) {}
 
   ngOnInit() {
@@ -30,10 +34,19 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-      const script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.innerHTML = 'addToHomescreen();';
-      this.elementRef.nativeElement.appendChild(script);
+      const addtohomescript = document.createElement('script');
+      addtohomescript.type = 'text/javascript';
+      addtohomescript.innerHTML = 'addToHomescreen();';
+      this.elementRef.nativeElement.appendChild(addtohomescript);
+
+      const lgscript = document.createElement('script');
+      lgscript.type = 'module';
+      lgscript.src = 'https://djjb.foehst.net/lehrgangsanmeldung.js';
+      this.elementRef.nativeElement.appendChild(lgscript);
+
+      let lglist = document.createElement('x-lehrgangsanmeldung');
+      lglist.style.cssText='font-size: .8em; overflow-y: auto;'
+      this.lghost.nativeElement.appendChild(lglist)
   }
 
   refreshFeed() {
